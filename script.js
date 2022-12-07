f5039c7e4492ad43d610e0a90c8efe06e1cf21ab
lpcapitalchi <119707098+lpcapitalchi@users.noreply.github.com>
2022-12-07 17:04:32 -0600

    Add styling and formatting

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 // Function to calculate the price of the user-entered amount of Bitcoin in US dollars
 function calculatePrice() {
   // Get the user-entered amount of Bitcoin from the input field
-  const btcAmount = document.getElementById('btc-amount').value;
+  const btcAmount = Number(document.getElementById('btc-amount').value);
 
   // Display the user-entered amount of Bitcoin on the page
-  document.getElementById('btc-amount-display').innerText = btcAmount;
+  document.getElementById('btc-amount-display').innerText = btcAmount.toLocaleString('en-US');
 
   // Fetch the current exchange rate from the API
   fetch(API_URL)
@@ -21,3 +21,17 @@ function calculatePrice() {
       document.getElementById('usd-price-display').innerText = satsPrice.toLocaleString('en-US', {maximumFractionDigits:0});
     });
 }
+
+// Get the input field
+var input = document.getElementById("btc-amount");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("calculate-button").click();
+  }
+});
